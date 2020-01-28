1525a84a14ef32af976a49699dcfe91a998dde00
SEKHOBANA <nessarefilwe@gmail.com>
2020-01-28 16:30:07 +0200

    changes

--- fruits-And-veg/src/app/app.module.ts
+++ fruits-And-veg/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+    {
+       path:"login",component:LoginComponent
+    }
+  ])
   ],
   providers: [],
   bootstrap: [AppComponent]
