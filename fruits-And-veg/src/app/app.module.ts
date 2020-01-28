73b6c33186c460e689a3e9f7feb18a49e8b0df54
ghass-del <sekgota5577@gmail.com>
2020-01-28 16:29:51 +0200

    added layouts

--- fruits-And-veg/src/app/app.module.ts
+++ fruits-And-veg/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { RegistrationComponent } from './registration/registration.component';
 import { RouterModule } from '@angular/router';
 import { SellerRegComponent } from './seller-reg/seller-reg.component';
 import { BuyerRegComponent } from './buyer-reg/buyer-reg.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
@@ -15,6 +16,7 @@ import { BuyerRegComponent } from './buyer-reg/buyer-reg.component';
     RegistrationComponent,
     SellerRegComponent,
     BuyerRegComponent,
+    HomeComponent 
     
   ],
   imports: [
@@ -23,7 +25,9 @@ import { BuyerRegComponent } from './buyer-reg/buyer-reg.component';
     RouterModule.forRoot([
       {path:"registration", component: RegistrationComponent},
       {path: "buyerReg", component: BuyerRegComponent},
-      {path: "sellerReg", component:SellerRegComponent}
+      {path: "sellerReg", component:SellerRegComponent},
+      {path: "home", component: HomeComponent}
+
     ])
   ],
   providers: [],
