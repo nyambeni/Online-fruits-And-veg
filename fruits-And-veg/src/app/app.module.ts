270063dabe083f319f983bdc50dc3ea50522ef82
Sphe <55232214+SiphesihleMabaso@users.noreply.github.com>
2020-01-29 10:03:25 +0200

    initial commit

--- fruits-And-veg/src/app/app.module.ts
+++ fruits-And-veg/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path:"admin",component:AdminComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
